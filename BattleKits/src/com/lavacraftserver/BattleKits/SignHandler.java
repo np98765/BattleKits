90c7ed28c963effaee01c573c3e2afa0257fb8c4
lol768 <support@technostripe.com>
2012-11-04 21:08:30 +0000

    SignHandler

---
+++ BattleKits/src/com/lavacraftserver/BattleKits/SignHandler.java
@@ -0,0 +1,33 @@
+package com.lavacraftserver.BattleKits;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+
+public class SignHandler implements Listener {
+	private BattleKits plugin;
+	
+	public SignHandler(BattleKits plugin) {
+		this.plugin = plugin;
+		
+	}
+	
+	@EventHandler
+	public void signEdit(SignChangeEvent e) {
+		String[] lines = e.getLines();
+		Player p = e.getPlayer();
+		if (lines.length > 1 && lines[0].equalsIgnoreCase("[BattleKits]")) {
+			if (plugin.getConfig().contains("kits." + lines[1])) {
+				e.setLine(0, ChatColor.DARK_RED + "[BattleKits]");
+				p.sendMessage(ChatColor.GREEN + "Kit sign created successfully.");
+			} else {
+				e.getBlock().breakNaturally();
+				p.sendMessage(ChatColor.RED + "Kit does not exist.");
+			}
+			
+		}
+	}
+
+}
