4c4d79d1ef475a213f9af4c914b5ab9e2294e11c
lol768 <support@technostripe.com>
2012-11-04 10:58:23 +0000

    Allow multiple enchantments on items and on armour. Only set armour once (not in the loop)

--- src/com/lavacraftserver/BattleKits/BattleKits.java
+++ src/com/lavacraftserver/BattleKits/BattleKits.java
@@ -7,6 +7,7 @@ import net.minecraft.server.NBTTagCompound;
 import net.minecraft.server.NBTTagList;
 import net.minecraft.server.NBTTagString;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -183,158 +184,209 @@ public class BattleKits extends JavaPlugin {
 											 
 										 }
 										 
-										 //Sets the enchantment and level
-										 if (s.length == 2 ) {
-											 String[] enchant = s[1].split(":");
-											 Enchantment enchantmentInt = new EnchantmentWrapper(Integer.parseInt(enchant[0]));
-											 int levelInt = Integer.parseInt(enchant[1]);
-											 i.addUnsafeEnchantment(enchantmentInt, levelInt);
-										 }
+										// Sets the enchantments and level
+										Boolean first = true;
+										if (s.length > 1) {
+											for (String a : s) {
+												if (!first) {
+
+													String[] enchant = a.split(":");
+													Enchantment enchantmentInt = new EnchantmentWrapper(Integer.parseInt(enchant[0]));
+													int levelInt = Integer.parseInt(enchant[1]);
+													i.addUnsafeEnchantment(enchantmentInt,levelInt);
+												}
+												first = false;
+											}
+										}
 										 
 										
 										 
-										 //Sets the armor contents
-										 String getHelmet = getConfig().getString("kits." + className + ".items" + ".helmet");
-										 String getChestplate = getConfig().getString("kits." + className + ".items" + ".chestplate");
-										 String getLeggings = getConfig().getString("kits." + className + ".items" + ".leggings");
-										 String getBoots = getConfig().getString("kits." + className + ".items" + ".boots");
-										 int helmColor = 0;
-										 int chestColor = 0;
-										 int legColor = 0;
-										 int bootColor = 0;
-										 
-										 ItemStack lhelmet = new ItemStack(Material.LEATHER_HELMET);
-										 ItemStack lchestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
-										 ItemStack lleggings = new ItemStack(Material.LEATHER_LEGGINGS);
-										 ItemStack lboots = new ItemStack(Material.LEATHER_BOOTS);
-										 
-										 ItemStack ihelmet = new ItemStack(Material.IRON_HELMET, 1);
-										 ItemStack ichestplate = new ItemStack(Material.IRON_CHESTPLATE, 1);
-										 ItemStack ileggings = new ItemStack(Material.IRON_LEGGINGS, 1);
-										 ItemStack iboots = new ItemStack(Material.IRON_BOOTS, 1);
-										 
-										 ItemStack ghelmet = new ItemStack(Material.GOLD_HELMET, 1);
-										 ItemStack gchestplate = new ItemStack(Material.GOLD_CHESTPLATE, 1);
-										 ItemStack gleggings = new ItemStack(Material.GOLD_LEGGINGS, 1);
-										 ItemStack gboots = new ItemStack(Material.GOLD_BOOTS, 1);
-										 
-										 ItemStack dhelmet = new ItemStack(Material.DIAMOND_HELMET, 1);
-										 ItemStack dchestplate = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
-										 ItemStack dleggings = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
-										 ItemStack dboots = new ItemStack(Material.DIAMOND_BOOTS, 1);
-										 
-										 ItemStack chelmet = new ItemStack(Material.CHAINMAIL_HELMET);
-										 ItemStack cchestplate = new ItemStack(Material.CHAINMAIL_CHESTPLATE);
-										 ItemStack cleggings = new ItemStack(Material.CHAINMAIL_LEGGINGS);
-										 ItemStack cboots = new ItemStack(Material.CHAINMAIL_BOOTS);
-										 
-										 if (getConfig().contains("kits." + className + ".items" + ".helmetColor"))
-										 {
-											  helmColor = Integer.parseInt(getConfig().getString("kits." + className + ".items.helmetColor").replace("#", ""), 16); 
-											  lhelmet = this.setColor(lhelmet, helmColor);
-											  
-										 }
-										 if (getConfig().contains("kits." + className + ".items" + ".chestplateColor"))
-										 {
-											  chestColor = Integer.parseInt(getConfig().getString("kits." + className + ".items.chestplateColor").replace("#", ""), 16);  
-											  lchestplate = this.setColor(lchestplate, chestColor);
-										 }
-										 if (getConfig().contains("kits." + className + ".items" + ".leggingColor"))
-										 {
-											  legColor = Integer.parseInt(getConfig().getString("kits." + className + ".items.leggingColor").replace("#", ""), 16); 
-											  lleggings = this.setColor(lleggings, legColor);
-										 }
-										 if (getConfig().contains("kits." + className + ".items" + ".bootColor"))
-										 {
-											  bootColor = Integer.parseInt(getConfig().getString("kits." + className + ".items.bootColor").replace("#", ""), 16);   
-											  lboots= this.setColor(lboots, bootColor);
-										 }
-										 
-										 
-									  
-										 if (getHelmet != null) {
-											 if (getHelmet.equals("leather")) {
-												 p.getInventory().setHelmet(lhelmet);
-											 }
-											 if (getHelmet.equals("iron")) {
-												 p.getInventory().setHelmet(ihelmet);
-											 }
-											 if (getHelmet.equals("gold")) {
-												 p.getInventory().setHelmet(ghelmet);
-											 }
-											 if (getHelmet.equals("diamond")) {
-												 p.getInventory().setHelmet(dhelmet);
-											 }
-											 if (getHelmet.equals("chainmail")) {
-												 p.getInventory().setHelmet(chelmet);
-											 }
-											 if (getHelmet.equals("none")) {
-												 p.getInventory().setHelmet(null);
-											 }
-										 }
-										 if (getChestplate != null) {
-											 if (getChestplate.equals("leather")) {
-												 p.getInventory().setChestplate(lchestplate);
-											 }
-											 if (getChestplate.equals("iron")) {
-												 p.getInventory().setChestplate(ichestplate);
-											 }
-											 if (getChestplate.equals("gold")) {
-												 p.getInventory().setChestplate(gchestplate);
-											 }
-											 if (getChestplate.equals("diamond")) {
-												 p.getInventory().setChestplate(dchestplate);
-											 }
-											 if (getChestplate.equals("chainmail")) {
-												 p.getInventory().setChestplate(cchestplate);
-											 }
-											 if (getChestplate.equals("none")) {
-												 p.getInventory().setChestplate(null);
-											 }
-										 }
-										 if (getLeggings != null) {
-											 if (getLeggings.equals("leather")) {
-												 p.getInventory().setLeggings(lleggings);
-											 }
-											 if (getLeggings.equals("iron")) {
-												 p.getInventory().setLeggings(ileggings);
-											 }
-											 if (getLeggings.equals("gold")) {
-												 p.getInventory().setLeggings(gleggings);
-											 }
-											 if (getLeggings.equals("diamond")) {
-												 p.getInventory().setLeggings(dleggings);
-											 }
-											 if (getLeggings.equals("chainmail")) {
-												 p.getInventory().setLeggings(cleggings);
-											 }
-											 if (getLeggings.equals("none")) {
-												 p.getInventory().setLeggings(null);
-											 }
-										 }
-										 if (getBoots != null) {
-											 if (getBoots.equals("leather")) {
-												 p.getInventory().setBoots(lboots);
-											 }
-											 if (getBoots.equals("iron")) {
-												 p.getInventory().setBoots(iboots);
-											 }
-											 if (getBoots.equals("gold")) {
-												 p.getInventory().setBoots(gboots);
-											 }
-											 if (getBoots.equals("diamond")) {
-												 p.getInventory().setBoots(dboots);
-											 }
-											 if (getBoots.equals("chainmail")) {
-												 p.getInventory().setBoots(cboots);
-											 }
-											 if (getBoots.equals("none")) {
-												 p.getInventory().setBoots(null);
-											 }
-										 }
 										 p.getInventory().setItem(slot, i);
 									 }
 								 }
+								 
+								//Sets the armor contents
+								 String getHelmet = getConfig().getString("kits." + className + ".items" + ".helmet");
+								 String getChestplate = getConfig().getString("kits." + className + ".items" + ".chestplate");
+								 String getLeggings = getConfig().getString("kits." + className + ".items" + ".leggings");
+								 String getBoots = getConfig().getString("kits." + className + ".items" + ".boots");
+								 int helmColor = 0;
+								 int chestColor = 0;
+								 int legColor = 0;
+								 int bootColor = 0;
+								 
+								 ItemStack lhelmet = new ItemStack(Material.LEATHER_HELMET);
+								 ItemStack lchestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
+								 ItemStack lleggings = new ItemStack(Material.LEATHER_LEGGINGS);
+								 ItemStack lboots = new ItemStack(Material.LEATHER_BOOTS);
+								 
+								 ItemStack ihelmet = new ItemStack(Material.IRON_HELMET, 1);
+								 ItemStack ichestplate = new ItemStack(Material.IRON_CHESTPLATE, 1);
+								 ItemStack ileggings = new ItemStack(Material.IRON_LEGGINGS, 1);
+								 ItemStack iboots = new ItemStack(Material.IRON_BOOTS, 1);
+								 
+								 ItemStack ghelmet = new ItemStack(Material.GOLD_HELMET, 1);
+								 ItemStack gchestplate = new ItemStack(Material.GOLD_CHESTPLATE, 1);
+								 ItemStack gleggings = new ItemStack(Material.GOLD_LEGGINGS, 1);
+								 ItemStack gboots = new ItemStack(Material.GOLD_BOOTS, 1);
+								 
+								 ItemStack dhelmet = new ItemStack(Material.DIAMOND_HELMET, 1);
+								 ItemStack dchestplate = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+								 ItemStack dleggings = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+								 ItemStack dboots = new ItemStack(Material.DIAMOND_BOOTS, 1);
+								 
+								 ItemStack chelmet = new ItemStack(Material.CHAINMAIL_HELMET);
+								 ItemStack cchestplate = new ItemStack(Material.CHAINMAIL_CHESTPLATE);
+								 ItemStack cleggings = new ItemStack(Material.CHAINMAIL_LEGGINGS);
+								 ItemStack cboots = new ItemStack(Material.CHAINMAIL_BOOTS);
+								 
+								 ItemStack finalHelmet = null;
+								 ItemStack finalChestplate = null;
+								 ItemStack finalLeggings = null;
+								 ItemStack finalBoots = null;
+								 if (getConfig().contains("kits." + className + ".items" + ".helmetColor"))
+								 {
+									  helmColor = Integer.parseInt(getConfig().getString("kits." + className + ".items.helmetColor").replace("#", ""), 16); 
+									  lhelmet = this.setColor(lhelmet, helmColor);
+									  
+									  
+								 }
+								 if (getConfig().contains("kits." + className + ".items" + ".chestplateColor"))
+								 {
+									  chestColor = Integer.parseInt(getConfig().getString("kits." + className + ".items.chestplateColor").replace("#", ""), 16);  
+									  lchestplate = this.setColor(lchestplate, chestColor);
+								 }
+								 if (getConfig().contains("kits." + className + ".items" + ".leggingColor"))
+								 {
+									  legColor = Integer.parseInt(getConfig().getString("kits." + className + ".items.leggingColor").replace("#", ""), 16); 
+									  lleggings = this.setColor(lleggings, legColor);
+								 }
+								 if (getConfig().contains("kits." + className + ".items" + ".bootColor"))
+								 {
+									  bootColor = Integer.parseInt(getConfig().getString("kits." + className + ".items.bootColor").replace("#", ""), 16);   
+									  lboots= this.setColor(lboots, bootColor);
+								 }
+								 
+						
+							  
+								 if (getHelmet != null) {
+									 if (getHelmet.equals("leather")) {
+										 finalHelmet = lhelmet;
+									 }
+									 if (getHelmet.equals("iron")) {
+										 finalHelmet = ihelmet;
+									 }
+									 if (getHelmet.equals("gold")) {
+										 finalHelmet = ghelmet;
+									 }
+									 if (getHelmet.equals("diamond")) {
+										 finalHelmet = dhelmet;
+									 }
+									 if (getHelmet.equals("chainmail")) {
+										 finalHelmet = chelmet;
+									 }
+									 
+								 }
+								
+								 if (getChestplate != null) {
+									 if (getChestplate.equals("leather")) {
+										 finalChestplate = lchestplate;
+									 }
+									 if (getChestplate.equals("iron")) {
+										 finalChestplate = ichestplate;
+									 }
+									 if (getChestplate.equals("gold")) {
+										 finalChestplate = gchestplate;
+									 }
+									 if (getChestplate.equals("diamond")) {
+										 finalChestplate = dchestplate;
+									 }
+									 if (getChestplate.equals("chainmail")) {
+										 finalChestplate = cchestplate;
+									 }
+									 
+								 }
+								 if (getLeggings != null) {
+									 if (getLeggings.equals("leather")) {
+										 finalLeggings = lleggings;
+									 }
+									 if (getLeggings.equals("iron")) {
+										 finalLeggings = ileggings;
+									 }
+									 if (getLeggings.equals("gold")) {
+										 finalLeggings = gleggings;
+									 }
+									 if (getLeggings.equals("diamond")) {
+										 finalLeggings = dleggings;
+									 }
+									 if (getLeggings.equals("chainmail")) {
+										 finalLeggings = cleggings;
+									 }
+								 }
+								 if (getBoots != null) {
+									 if (getBoots.equals("leather")) {
+										 finalBoots = lboots;
+									 }
+									 if (getBoots.equals("iron")) {
+										 finalBoots = iboots;
+									 }
+									 if (getBoots.equals("gold")) {
+										 finalBoots = gboots;
+									 }
+									 if (getBoots.equals("diamond")) {
+										 finalBoots = dboots;
+									 }
+									 if (getBoots.equals("chainmail")) {
+										 finalBoots = cboots;
+									 }
+									 
+								 }
+								 
+								 if (getConfig().contains("kits." + className + ".items.helmetEnchant") && finalHelmet != null) {
+									  for (String a : getConfig().getString("kits." + className + ".items.helmetEnchant").split(" ")) {
+
+												String[] enchant = a.split(":");
+												Enchantment enchantmentInt = new EnchantmentWrapper(Integer.parseInt(enchant[0]));
+												int levelInt = Integer.parseInt(enchant[1]);
+												finalHelmet.addUnsafeEnchantment(enchantmentInt,levelInt);
+										}
+								  }
+								 
+								
+								 
+								 if (getConfig().contains("kits." + className + ".items.chestplateEnchant") && finalChestplate != null) {
+									  for (String a : getConfig().getString("kits." + className + ".items.chestplateEnchant").split(" ")) {
+
+												String[] enchant = a.split(":");
+												Enchantment enchantmentInt = new EnchantmentWrapper(Integer.parseInt(enchant[0]));
+												int levelInt = Integer.parseInt(enchant[1]);
+												finalChestplate.addUnsafeEnchantment(enchantmentInt,levelInt);
+										}
+								  }
+								 
+								 if (getConfig().contains("kits." + className + ".items.leggingsEnchant") && finalLeggings != null) {
+									  for (String a : getConfig().getString("kits." + className + ".items.leggingsEnchant").split(" ")) {
+
+												String[] enchant = a.split(":");
+												Enchantment enchantmentInt = new EnchantmentWrapper(Integer.parseInt(enchant[0]));
+												int levelInt = Integer.parseInt(enchant[1]);
+												finalLeggings.addUnsafeEnchantment(enchantmentInt,levelInt);
+										}
+								  }
+								 
+								 if (getConfig().contains("kits." + className + ".items.bootsEnchant") && finalBoots != null) {
+									  for (String a : getConfig().getString("kits." + className + ".items.bootsEnchant").split(" ")) {
+												String[] enchant = a.split(":");
+												Enchantment enchantmentInt = new EnchantmentWrapper(Integer.parseInt(enchant[0]));
+												int levelInt = Integer.parseInt(enchant[1]);
+												finalBoots.addUnsafeEnchantment(enchantmentInt,levelInt);
+										}
+								  }
+								 
+								 p.getInventory().setHelmet(finalHelmet);
+								 p.getInventory().setChestplate(finalChestplate);
+								 p.getInventory().setLeggings(finalLeggings);
+								 p.getInventory().setBoots(finalBoots);
 								 
 								 if (getConfig().getBoolean("settings.once-per-life")) {
 									 getConfig().set("dead." + p.getName(), true);
