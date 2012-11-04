7ad17583cfddd85e63160f7e72eb9270d0f03d85
lol768 <support@technostripe.com>
2012-11-04 20:08:01 +0000

    Add simple soup command

--- BattleKits/src/com/lavacraftserver/BattleKits/CommandSoup.java
+++ BattleKits/src/com/lavacraftserver/BattleKits/CommandSoup.java
@@ -1,8 +1,14 @@
 package com.lavacraftserver.BattleKits;
 
+
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 
 public class CommandSoup implements CommandExecutor {
@@ -18,7 +24,19 @@ public class CommandSoup implements CommandExecutor {
 		
 		//Assigned to np98765, so I've only made the command class
 		if (cmd.getName().equals("soup")) {
-			// TODO Write soup command
+			if (sender.hasPermission("BattleKits.soup")) {
+				Player p = (Player) sender;
+				ItemStack i = p.getItemInHand();
+				if (i.getType() != Material.BOWL) {
+					sender.sendMessage(ChatColor.RED + "You must have an empty bowl in your hand");
+					return true;
+				} else {
+					i.setType(Material.MUSHROOM_SOUP);
+			}
+			} else {
+				sender.sendMessage(ChatColor.RED + "You don't have permission for this command.");
+
+			}
 			
 		}
 		return false;
