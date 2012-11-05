d34d9ea1276b43f4eaad0d0fa523f1fd5273e7c6
lol768 <support@technostripe.com>
2012-11-05 16:21:07 +0000

    Support CommandSender to save cast to Player

--- BattleKits/src/com/lavacraftserver/BattleKits/PM.java
+++ BattleKits/src/com/lavacraftserver/BattleKits/PM.java
@@ -1,6 +1,7 @@
 package com.lavacraftserver.BattleKits;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class PM {
@@ -13,6 +14,14 @@ public class PM {
 
 	public void warn(Player player, String message) {
 		player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "BattleKits" + ChatColor.GRAY + "] " + ChatColor.RED + message);
+	}
+	
+	public void warn(CommandSender player, String message) {
+		player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "BattleKits" + ChatColor.GRAY + "] " + ChatColor.RED + message);
+	}
+	
+	public void notify(CommandSender player, String message) {
+		player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "BattleKits" + ChatColor.GRAY + "]" + ChatColor.YELLOW + message);
 	}
 	
 	public void notify(Player player, String message) {
