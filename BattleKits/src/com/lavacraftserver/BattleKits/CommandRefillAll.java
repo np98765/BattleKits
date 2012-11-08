9bb90b9cf7db9d0d3a747fa2e7c2d23c42f4ca44
lol768 <support@technostripe.com>
2012-11-08 20:57:42 +0000

    Fore jenkins build

--- BattleKits/src/com/lavacraftserver/BattleKits/CommandRefillAll.java
+++ BattleKits/src/com/lavacraftserver/BattleKits/CommandRefillAll.java
@@ -34,7 +34,7 @@ public class CommandRefillAll implements CommandExecutor {
 			if (sender.hasPermission("BattleKits.use.fillall")) {
 				//Get array of itemstack
 				for (ItemStack slot : inv) {
-				    if (slot != null && slot.getType() == Material.BOWL) { //Check for NPE
+				    if (slot != null && slot.getType() == Material.BOWL) { //Check for NPE 
 				    	gotBowl = true;
 				    	slot.setType(Material.MUSHROOM_SOUP);
 				    }
